26fa87960954d007487e36102d1b8761422ff0b7
Manish V <imvmanish18@gmail.com>
2021-11-03 22:37:54 +0530

    used event delegation

--- app.js
+++ app.js
@@ -7,19 +7,21 @@ let state = 0;
 
 function addTodo() {
     let liElement = document.createElement('li');
+    let checkBox = document.createElement('input');
     let spanElement = document.createElement('span');
     let editElement = document.createElement('i');
     let deleteElement = document.createElement('i');
     let upElement = document.createElement('i');
     let downElement = document.createElement('i');
 
+    checkBox.type = "checkbox";
     spanElement.textContent = inpElement.value;
-    console.log(spanElement);
     editElement.classList.add('far','fa-edit');
     deleteElement.classList.add('far','fa-trash-alt');
     upElement.classList.add('fas','fa-arrow-up');
     downElement.classList.add('fas','fa-arrow-down');
 
+    liElement.appendChild(checkBox);
     liElement.appendChild(spanElement);
     liElement.appendChild(editElement);
     liElement.appendChild(deleteElement);
@@ -27,7 +29,9 @@ function addTodo() {
     liElement.appendChild(downElement);
 
     pendingTasksList.appendChild(liElement);
+    inpElement.value = "";
 }
+
 
 submitBtn.addEventListener('click',function() {
     if(inpElement.value == "") {
@@ -37,4 +41,19 @@ submitBtn.addEventListener('click',function() {
     if(state == 0) {
         addTodo();
     }
+});
+
+pendingTasksList.addEventListener('click',function(e) {
+    let work;
+    let targetElement = e.target;
+    if(targetElement.classList.contains('fa-edit')) {
+        work = 1;
+    } else if(targetElement.classList.contains('fa-trash-alt')) {
+        work = 2;
+    } else if(targetElement.classList.contains('fa-arrow-up')) {
+        work = 3;
+    } else if(targetElement.classList.contains('fa-arrow-down')) {
+        work = 4;
+    }
+    console.log(work);
 });
